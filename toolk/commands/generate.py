51114b8384b2c13507ecaec71f41a69c91c40442
heinrich <heinrich@vanhein.com>
2017-07-18 03:14:37 -0400

    find or create group in xcode.

--- toolk/commands/generate.py
+++ toolk/commands/generate.py
@@ -5,10 +5,11 @@
 from json import dumps
 from .base import Base
 import sys
-import os
 import glob
 import pathlib
-from pbxproj import XcodeProject
+import os
+import re
+import uuid
 
 from .messages import headerString
 from .messages import pathError
@@ -34,9 +35,28 @@ class Generate(Base):
             response = input(vcConfirmation.replace('{}',name))
             self.check_response(response)
 
+            # Open template.xcodeproj/project.pbxproj
+            path = '{}/project.pbxproj'.format(glob.glob('*.xcodeproj')[0])
+            project_file = open(path,'r')
+            project = project_file.read()
 
-            print("done!")
+            # Check if Controller group exists
+            controller_string = """name = Controllers;
+			sourceTree = "<group>";"""
+            if controller_string in project:
+                print(" ✅  Controllers folder founded!")
+            else:
+                self.create_project_folder(project, "Controllers")
 
+
+            # Create the file
+            filename = "./controllers/file.swift"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, "w") as f:
+                f.write("FOOBAR")
+
+            # Create the viewcontroller folder if needed
+            # pathlib.Path('/my/directory').mkdir(parents=True, exist_ok=True)
 
     def check_path(self):
         """Check if the commandline is in the appropriate project path"""
@@ -46,7 +66,6 @@ class Generate(Base):
             print(pathError)
             sys.exit()
 
-
     def check_response(self, response):
         """Check if the answer is yes or no"""
         if response == "Y" or response == "y" or response == "YES" or response == "yes" or response == "Yes":
@@ -54,3 +73,52 @@ class Generate(Base):
         else:
             print(exitMessage)
             sys.exit()
+
+    def create_project_folder(self, project, folder_name):
+        print(" ❗  {} folder doesn't exist yet in Xcode project.".format(folder_name))
+
+        # Generate id
+        group_id = self.generate_id()
+
+        # Build the group block
+        group_string = """\
+        <group_id> /* <folder_name> */ = {
+			isa = PBXGroup;
+			children = ();
+			name = <folder_name>;
+			sourceTree = "<group>";
+		};
+        """
+        group_string = group_string.replace('<group_id>', group_id)
+        group_string = group_string.replace('<folder_name>', folder_name)
+
+        # Append the block into the project
+        query = "/* End PBXGroup section */"
+        project = self.insert_string(project, group_string, project.rfind(query))
+
+        # Find the name of the project
+        pattern = r'remoteInfo = (.*);'
+        match = re.search(pattern, project)
+        print(" ✅  {} is the parent folder.".format(match.group(1)))
+
+        # Find the main folder
+        pattern = r'/\* '+match.group(1)+' \*/ = {.*children = \((.*)\).*path = '+match.group(1)+';'
+        match = re.search(pattern, project, re.DOTALL)
+
+        # Append group to the main folder
+        group_string = "\n{} /* {} */,".format(group_id, folder_name)
+        project = self.insert_string(project, group_string, match.span(1)[0])
+
+        # Save project
+        path = '{}/project.pbxproj'.format(glob.glob('*.xcodeproj')[0])
+        with open(path, "w") as f:
+            f.write(project)
+
+        print(" ✅  {} folder created in Xcode project.".format(folder_name))
+
+
+    def generate_id(self):
+        return ''.join(str(uuid.uuid4()).upper().split('-')[1:])
+
+    def insert_string(self, string, string_to_add, index):
+        return string[:index] + string_to_add + string[index:]
